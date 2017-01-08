a372abe1df3e8b5003d4c550f75d2c0886ad0ae9
b-gran <bill@billgranfield.com>
2017-01-07 23:25:09 -0500

    Creates RecordSchema class to replace FieldValue objects

---
+++ src/RecordSchema.js
@@ -0,0 +1,407 @@
+import _ from 'lodash';
+
+/**
+ * Possible string results of the typeof operator.
+ * @type {String[]}
+ */
+const TYPE_STRINGS = [
+  'object', 'string', 'number', 'symbol', 'boolean', 'function', 'undefined'
+];
+
+const privates = new WeakMap();
+
+class RecordSchema {
+  constructor (schema) {
+    // Throw if the schema is invalid
+    validateRecordSchema(schema);
+
+    privates.set(this, Object.freeze(schema));
+  }
+
+  /**
+   * Given a property name, returns true if the property name is specified
+   * in the schema.
+   *
+   * @param {String} propertyName
+   * @return {boolean}
+   */
+  hasProperty (propertyName) {
+    return propertyName in privates.get(this);
+  }
+
+  /**
+   * Given a potential input for a Record, removes any keys from the input
+   * that aren't present on the schema.
+   *
+   * @param {Object} input
+   * @return {Object}
+   */
+  removeInvalidInputKeys (input) {
+    const schema = privates.get(this);
+
+    return _.pick(
+      input,
+      Object.keys(schema)
+    );
+  }
+
+  /**
+   * Given a potential input for a Record, validates the input.
+   * Specifically, tests each key/value of the input and throws if one of the pairs
+   * is invalid according to the Schema.
+   *
+   * @param {Object} input
+   * @return {boolean}
+   */
+  validateInput (input) {
+    const schema = privates.get(this);
+
+    if (!_.isNil(input) && !_.isPlainObject(input)) {
+      throw new Error(`Record input must either be nil or a plain object.`);
+    }
+
+    return _.every(
+      _.keys(schema),
+      key => validateInputValue(key, schema, input)
+    );
+  }
+
+  /**
+   * Given a potential input for a Record, replaces any not-present values with
+   * the value specified by the Schema. Does nothing to defined values.
+   *
+   * @param {Object} input
+   * @return {Object}
+   */
+  applyDefaults (input) {
+    const schema = privates.get(this);
+
+    return _.defaults(
+      input,
+      _.mapValues(
+        _.pickBy(
+          schema,
+          schemaValue => 'default' in schemaValue
+        ),
+        _.property('default')
+      )
+    );
+  }
+
+  /**
+   * Given a Record input, returns an accessor object (suitable for use
+   * with Object.defineProperties) for the input values.
+   *
+   * @param {Object} input
+   * @return {{}<RecordAccessor>}
+   */
+  getAccessorsForInput (input) {
+    const schema = privates.get(this);
+
+    return _.mapValues(
+      _.pick(
+        input,
+        Object.keys(schema)
+      ),
+      (value, key) => RecordAccessor(value)
+    );
+  }
+}
+
+/**
+ * Given a key name, a schema, and some a potential input for a Record, tests the value
+ * at the key in the input for validity according to the schema.
+ *
+ * Specifically, throws if the value isn't valid according to the schema.
+ *
+ * @param {String} key
+ * @param {RecordSchema} schema
+ * @param {Object} recordInput
+ * @return {boolean}
+ */
+function validateInputValue (key, schema, recordInput) {
+  // We treat nil input (which is potentially valid) as an empty object.
+  recordInput = _.isNil(recordInput) ? {} : recordInput;
+
+  const schemaValue = schema[key];
+
+  // Check for required fields (that also don't have a default)
+  if (!(key in recordInput)) {
+    if (isFieldRequired(key, schema)) {
+      throw new Error(
+        `"${key}" is missing from the record ${JSON.stringify(recordInput)}.`
+      );
+    }
+
+    // Key is optional & not supplied
+    return true;
+  }
+
+  // If the type is unspecified, we're done.
+  if (!('type' in schemaValue)) {
+    return true;
+  }
+
+  // Check the type
+  const inputValue = recordInput[key];
+  if (!isValidForType(schemaValue.type, inputValue)) {
+    throw new Error(
+      `The value ${JSON.stringify(inputValue)} at "${key}" is invalid.`
+    );
+  }
+
+  return true;
+}
+
+/**
+ * Given a key (field name) and a schema, returns true if the key must be specified on
+ * Record input.
+ *
+ * A key doesn't need to be specified if either:
+ *    required is falsey
+ *    a default value is provided
+ *
+ * @param {String} key
+ * @param {RecordSchema} schema
+ * @return {Boolean}
+ */
+function isFieldRequired (key, schema) {
+  const schemaValue = schema[key];
+
+  return (
+    schemaValue.required &&
+    !('default' in schemaValue)
+  );
+}
+
+/**
+ * Returns true if a given object is a valid RecordSchema.
+ * Throws a RecordSchemaValidationError if the schema is invalid.
+ *
+ * @param {*} recordSchema
+ * @return {boolean}
+ */
+function validateRecordSchema (recordSchema) {
+  if (!_.isPlainObject(recordSchema)) {
+    throw new Error('A RecordSchema must be a plain object.')
+  }
+
+  if (isEmptyObject(recordSchema)) {
+    throw new Error(`A RecordSchema must be a plain object with 0 enumerable keys.`)
+  }
+
+  // Every value must be individually valid
+  return _.every(
+    recordSchema,
+    isRecordSchemaValue
+  );
+}
+
+/**
+ * A RecordAccessor:
+ *   has a getter that returns the input value
+ *   has a setter that throws
+ *   may or may not be enumerable
+ *   is not configurable
+ *
+ * RecordAccessors are intended for use with Object.defineProperty()
+ *
+ * @property {function} get
+ * @property {function} set
+ * @property {boolean} enumerable
+ * @property {boolean} configurable
+ *
+ * @param {ValueType} value - the value to provide accessors for
+ * @param {Boolean} isEnumerable - whether or not the property is enumerable
+ * @return {{get: (function(): ValueType), set: (function(*)), enumerable: boolean}}
+ * @template ValueType
+ */
+function RecordAccessor (value, isEnumerable = true) {
+  return {
+    get: () => value,
+
+    // Always throw on attempted set operations.
+    // This function has arity of one to comply with interpreter rules about setters,
+    // even though it won't ever use the argument.
+    set: x => {
+      throw new Error('Use the "set" function to update the values of an ImmutableRecord.')
+    },
+
+    enumerable: isEnumerable,
+    configurable: false
+  }
+}
+
+/**
+ * Returns true if an object is "empty". Any empty object either:
+ *
+ *    1. is plain and has 0 enumerable keys.
+ *    2. is null or undefined
+ *
+ * @param {*} object - the value to test
+ * @return {boolean}
+ */
+function isEmptyObject (object) {
+  return (
+    _.overEvery(
+      _.isPlainObject,
+      _.isEmpty
+    )(object) ||
+    _.isNil(object)
+  );
+}
+
+/**
+ * RecordSchemas are plain objects whose values are also plain objects that must conform to
+ * a specific format. The RecordSchema values may only contain the following enumerable keys:
+ *
+ *    'type': either
+ *        1. A TYPE_STRING
+ *        2. A function of arity 1
+ *
+ *    'default' any value
+ *
+ *    'required': a Boolean
+ *
+ * Additionally, if a type and default are both specified then the default value must validate
+ * according to the type.
+ *
+ * @param {*} recordSchemaValue
+ * @return {boolean}
+ */
+function isRecordSchemaValue (recordSchemaValue) {
+  if (isEmptyObject(recordSchemaValue)) {
+    return true;
+  }
+
+  // Any enumerable key other than 'type', 'default', or 'required' is invalid
+  if (doesSchemaValueContainInvalidKeys(recordSchemaValue)) {
+    throw new Error(`${JSON.stringify(recordSchemaValue)} contains an invalid key.`);
+  }
+
+  // Test type, default, and required for individual correctness.
+  // Each value can be undefined.
+  const individuallyCorrectValues = conformsExistingKeys({
+    'type': function (typeValue) {
+      if (isTypeString(typeValue) || isValidatorFunction(typeValue)) {
+        return true;
+      }
+
+      throw new Error(`"type" is invalid.`);
+    },
+
+    'default': function (defaultValue) {
+      return true;
+    },
+
+    'required': function (requiredValue) {
+      if (_.isBoolean(requiredValue)) {
+        return true;
+      }
+
+      throw new Error(`"required" is invalid.`)
+    }
+  })(recordSchemaValue);
+
+  if (!individuallyCorrectValues) {
+    // Shouldn't get here.
+    throw new Error(`${JSON.stringify(recordSchemaValue)} contains an invalid value.`);
+  }
+
+  // If it doesn't have a type AND a default, we're done.
+  if (!('type' in recordSchemaValue) || !('default' in recordSchemaValue)) {
+    return true;
+  }
+
+  // If type and default are both specified, make sure the default validates
+  // according to the type.
+  if (!isValidForType(recordSchemaValue.type, recordSchemaValue.default)) {
+    throw new Error(`The default value is not valid according to the type.`);
+  }
+
+  return true;
+}
+
+/**
+ * Just like lodash's _.conforms, but only tests the predicates in source
+ * if the object actually has the key.
+ *
+ * @param {Object} source
+ * @return {function(Object): Boolean}
+ */
+function conformsExistingKeys (source) {
+  if (!_.isPlainObject(source)) {
+    throw new Error(`source must be a plain object.`);
+  }
+
+  return object => {
+    const specForObject = _.pick(source, Object.keys(object));
+    return _.conforms(specForObject)(object);
+  };
+}
+
+/**
+ * Given a Schema value, returns true if the value contains enumerable keys other than
+ *
+ *    type, default, and required
+ *
+ * @param {Object} schemaValue
+ * @return {boolean}
+ */
+function doesSchemaValueContainInvalidKeys (schemaValue) {
+  return (
+    _.difference(
+      _.keys(schemaValue),
+      [ 'type', 'default', 'required' ]
+    ).length > 0
+  );
+}
+
+/**
+ * Given a type and some potential value, returns true if the value is a valid
+ * instance of the type.
+ *
+ * @param {String|Function|undefined} type - type of the value
+ * @param {*} value - the value to check
+ * @return {boolean}
+ */
+function isValidForType (type, value) {
+  switch (typeof type) {
+    // If the definition has no type, every value is valid.
+    case 'undefined':
+      return true;
+
+    // If type is a string, we just make sure 'typeof value' matches the type.
+    case 'string':
+      return typeof value === type;
+
+    // If type is a function (it's a validator function), we pass in value and
+    // return the result (coerced to a boolean).
+    case 'function':
+      return !!type(value);
+
+    default:
+      throw new Error(`The type ${type} is not a valid type definition.`);
+  }
+}
+
+function isTypeString (typeString) {
+  return (
+    _.isString(typeString) &&
+    _.includes(TYPE_STRINGS, typeString.toLowerCase())
+  )
+}
+
+function isValidatorFunction (validatorFunction) {
+  return (
+    _.isFunction(validatorFunction) && validatorFunction.length === 1
+  )
+}
+
+function RecordSchemaValidationError (message) {
+  Error.call(this, message);
+  this.name = 'RecordSchemaValidationError';
+  this.message = message;
+}
+
+export default RecordSchema;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import FieldValue from './FieldValue';
+import RecordSchema from './RecordSchema';
 
 /**
  * Returns a Record class based on the shape supplied to this function.
@@ -9,44 +9,27 @@ import FieldValue from './FieldValue';
  * @return {Record}
  */
 function ImmutableRecord (shape, name) {
-  if (!shapeHasFields(shape)) {
-    throw new Error('shape must be a plain object with at least one field.')
-  }
-
-  // Convert each of the value definitions to FieldValue:s.
-  // Note: this will throw if any of the definitions are invalid.
-  const recordShape = _.mapValues(
-    shape,
-    valueDefinition => new FieldValue(valueDefinition)
-  );
+  // Initialize a new schema based on the supplied shape
+  const schema = new RecordSchema(shape);
 
   // A store for private variables
   const privates = new WeakMap();
 
-  // TODO: create a RecordShape class to encapsulate all of this functionality
-
-  // Initialize accessors function so we can add accessors to records
-  const accessors = bindAccessors(recordShape);
-
-  // Initialize defaults function so we can clean input.
-  const defaults = bindRecordDefaults(recordShape);
-
-  // Initialize input validation function.
-  const validateInput = bindValidateInput(recordShape);
-
   // Create a class with this specific shape
   function Record (values) {
-    // Clean input & add defaults
-    const cleanInput = defaults(values);
+    // Validate input
+    schema.validateInput(values);
 
-    // Validate
-    validateInput(cleanInput);
+    // Clean input & add defaults
+    const cleanInput = schema.applyDefaults(
+      schema.removeInvalidInputKeys(values)
+    );
 
     // Store private vars
     privates.set(this, cleanInput);
 
     // Add accessors to self
-    Object.defineProperties(this, accessors(cleanInput));
+    Object.defineProperties(this, schema.getAccessorsForInput(cleanInput));
   }
 
   // Update the record's name
@@ -68,7 +51,7 @@ function ImmutableRecord (shape, name) {
    * @return {Record}
    */
   Record.prototype.set = function (property, newValue) {
-    assertValidProperty(recordShape, property);
+    assertValidProperty(schema, property);
 
     return new Record(
       update(
@@ -92,7 +75,7 @@ function ImmutableRecord (shape, name) {
    * @return {Record}
    */
   Record.prototype.remove = function (property) {
-    assertValidProperty(recordShape, property);
+    assertValidProperty(schema, property);
 
     // TODO: better way to do this?
     return new Record(
@@ -122,151 +105,6 @@ function ImmutableRecord (shape, name) {
 }
 
 /**
- * Given the shape of a Record, returns a function that takes an input object and replaces
- * any fields that aren't present on the input with the Record's defaults.
- *
- * Also removes any fields from the input that aren't present on the shape.
- *
- * @param {{}.<FieldValue>} recordShape - the shape of the Record
- * @return {function({}): {}}
- */
-function bindRecordDefaults (recordShape) {
-  // Store the defaults and keys in a closure
-  const recordDefaults = getDefaults(recordShape);
-  const validKeys = Object.keys(recordShape);
-
-  return recordInput => {
-    // Remove any invalid keys
-    const prunedValues = _.pick(recordInput, validKeys);
-
-    // Add defaults when a value is missing from the input
-    return _.defaults(
-      prunedValues,
-      recordDefaults
-    );
-  };
-}
-
-/**
- * Given a record shape, returns a function that accepts record values and returns accessors
- * for the record properties that are present on the shape.
- *
- * @param {Object} recordShape - shape of the record
- * @return {function(Object): Object}
- */
-function bindAccessors (recordShape) {
-  return recordInput => {
-    return _.reduce(
-      Object.keys(recordShape),
-      (properties, fieldName) => {
-        // Only create an accessor for the property if it's present on the input.
-        if (!(fieldName in recordInput)) {
-          return properties;
-        }
-
-        // Create a getter for this specific fieldName & value.
-        // The setter for this property will throw.
-        const getter = {
-          [fieldName]: createAccessorWithValue(recordInput[fieldName], true)
-        };
-
-        // Add it to the properties object.
-        return _.assign(properties, getter);
-      },
-      {}
-    )
-  }
-}
-
-/**
- * Given a record shape, returns a function that accepts record values and throws if the record
- * values don't validate according to the shape.
- *
- * @param {Object} recordShape - shape of the record
- * @return {function(Object): void}
- */
-function bindValidateInput (recordShape) {
-  // TODO: could be inefficient
-  return recordInput => {
-    Object.keys(recordShape).forEach(
-      fieldName => {
-        const fieldValue = recordShape[fieldName];
-        const inputValue = recordInput[fieldName];
-
-        // Check for required fields
-        if (fieldValue.required && !(fieldName in recordInput)) {
-          // TODO: this warns with the cleaned up input, so you may not get useful info
-          throw new Error(
-            `"${fieldName}" is missing from the record ${JSON.stringify(recordInput)}.`
-          );
-        }
-
-        // Check for correct fields
-        if (!fieldValue.isValid(inputValue)) {
-          throw new Error(
-            `The value ${JSON.stringify(inputValue)} at "${fieldName}" is invalid.`
-          );
-        }
-
-        // Otherwise succeed
-      }
-    )
-  }
-}
-
-/**
- * Given the shape of a record, return an object containing only the default values specified
- * for the record.
- *
- * If the record has no defaults specified, {} will be returned.
- *
- * @param {{}.<FieldValue>} recordShape - shape of the Record
- * @return {{}.<*>}
- */
-function getDefaults (recordShape) {
-  return _.mapValues(
-    // Only want fields that have a default value
-    _.pickBy(
-      recordShape,
-      fieldValue => fieldValue.hasDefault()
-    ),
-
-    // Select the default value
-    fieldValue => fieldValue.default
-  )
-}
-
-
-/**
- * Given some value, return an accessor for that value.
- *
- * The accessors have the following properties:
- *    1. getter simply returns the record value
- *    2. setter always throws
- *
- * @param {ValueType} value - the value to provide accessors for
- * @param {Boolean} isEnumerable - whether or not the property is enumerable
- * @return {{get: (function(): ValueType), set: (function(*)), enumerable: boolean}}
- *
- * @template ValueType
- */
-function createAccessorWithValue (value, isEnumerable = true) {
-  return {
-    get: () => value,
-
-    // Always throw on attempted set operations.
-    // This function has arity of one to comply with interpreter rules about setters,
-    // even though it won't ever use the argument.
-    set: x => {
-      throw new Error('Use the "set" function to update the values of an ImmutableRecord.')
-    },
-
-    enumerable: isEnumerable,
-    configurable: false
-  }
-}
-
-/**
  * Immutably update a single top-level field of a plain object.
  *
  * Specifically: given a plain object, a field name, and a value, returns a new object identical
@@ -291,32 +129,15 @@ function update (object, field, value) {
 }
 
 /**
- * Returns true if the input object is a valid "shape" object -- one that defines the shape of an
- * ImmutableRecord.
- *
- * A shape object:
- *   * is a plain object
- *   * has one or more keys
- *   * no restrictions placed on values
- *
- * @param {*} object
- * @return {boolean}
- */
-function shapeHasFields (object) {
-  // TODO: should be handled by RecordShape
-  return _.isPlainObject(object) && Object.keys(object).length > 0;
-}
-
-/**
  * Given a Record shape and a property name, returns true if the property is in the shape and
  * throws otherwise.
  *
- * @param {{}.<FieldValue>} recordShape - the shape of the Record
+ * @param {RecordSchema} schema - Record's Schema
  * @param {String} property - the property name
  * @return {boolean}
  */
-function assertValidProperty (recordShape, property) {
-  if (!(property in recordShape)) {
+function assertValidProperty (schema, property) {
+  if (!schema.hasProperty(property)) {
     throw new Error(`"${property}" is not a valid field.`)
   }
 
